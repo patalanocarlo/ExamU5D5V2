0caebe5ec1b4ce1090d443e052a91089d815b812
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-06-28 09:54:21 +0200

    Initial commit

---
+++ src/main/java/PatalanoCarlo/ExamU5D5V2/ExamU5D5V2Application.java
@@ -0,0 +1,13 @@
+package PatalanoCarlo.ExamU5D5V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamU5D5V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamU5D5V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/PatalanoCarlo/ExamU5D5V2/ExamU5D5V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package PatalanoCarlo.ExamU5D5V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamU5D5V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
