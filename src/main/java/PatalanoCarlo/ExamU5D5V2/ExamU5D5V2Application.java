bc639858dd71cd39c6347b6f943e36c347a39dab
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-06-28 15:47:41 +0200

    Update ExamU5D5V2Application.java

--- src/main/java/PatalanoCarlo/ExamU5D5V2/ExamU5D5V2Application.java
+++ src/main/java/PatalanoCarlo/ExamU5D5V2/ExamU5D5V2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamU5D5V2Application {
+public class  ExamU5D5V2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamU5D5V2Application.class, args);
